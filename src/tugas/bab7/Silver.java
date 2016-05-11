9b6bb1b558abceab1684714b68c90ae0296dc78b
wenigustirahayu <wenigr97@gmail.com>
2016-05-11 23:26:56 +0700

    class Silver

---
+++ src/tugas/bab7/Silver.java
@@ -0,0 +1,31 @@
+package BAB7;
+
+public class Silver extends Member {
+
+    double diskon;
+
+    public Silver(String produk, String jasa, double hargaProduk, double hargajasa, String nama) {
+        super(produk, jasa, hargaProduk, hargajasa, nama);
+    }
+
+    public double diskonLayanan() {
+        return 0.1;
+    }
+
+    public double diskonProduk() {
+        return super.diskonProduk();
+    }
+
+    public void Salon() {
+        super.Salon();
+        System.out.println("Anda terdaftar sebagai Member SILVER");
+        System.out.println("Diskon Pelayanan sebesar 10%");
+        System.out.println("Diskon Produk sebesar 10%");
+    }
+
+    public double biaya() {
+        double total;
+        total = super.biaya() - super.getHargaPelayanan() * diskonLayanan();
+        return total;
+    }
+}
