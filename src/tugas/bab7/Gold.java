0c326ea09acaa764248b7cb39ce677638aaae3b4
wenigustirahayu <wenigr97@gmail.com>
2016-05-11 23:19:43 +0700

    class gold

---
+++ src/tugas/bab7/Gold.java
@@ -0,0 +1,26 @@
+package BAB7;
+public class Gold extends Member {
+    double diskon;
+
+    public Gold(String produk, String jasa, double hargaProduk, double hargajasa, String nama) {
+        super(produk, jasa, hargaProduk, hargajasa, nama);
+    }
+   
+    public double diskonLayanan(){
+        return 0.15;
+    }
+    public double diskonProduk(){
+        return super.diskonProduk();
+    }
+    public void Salon(){
+        super.Salon();
+        System.out.println("Anda terdaftar sebagai Member GOLD");
+        System.out.println("-Diskon Pelayanan sebesar 15%");
+        System.out.println("-Diskon Produk sebesar 10%");
+    }    
+    public double biaya(){
+        double total;
+        total = super.biaya()- super.getHargaPelayanan()*diskonLayanan();
+        return total;
+    }
+ }
