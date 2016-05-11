0a509a014e754cd506d92469c7679c743a4cde10
wenigustirahayu <wenigr97@gmail.com>
2016-05-11 23:30:08 +0700

    class Member

---
+++ src/tugas/bab7/Member.java
@@ -0,0 +1,27 @@
+package BAB7;
+
+public class Member extends Salon {
+
+    public Member(String produk, String jasa, double hargaProduk, double hargalayanan, String nama) {
+        super(produk, jasa, hargaProduk, hargalayanan, nama);
+    }
+
+    public double diskonProduk() {
+        return 0.1;
+    }
+
+    public void Salon() {
+        System.out.println("Anda Member");
+    }
+
+    @Override
+    public double diskonLayanan() {
+        return 0;
+    }
+
+    public double biaya() {
+        double total;
+        total = super.biaya() - super.getHargaProduk() * diskonProduk();
+        return total;
+    }
+}
